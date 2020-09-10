cd13dbd11502e4105999468396704204091d93b0
DJ-Jackson <jacdouglass@gmail.com>
2020-09-10 14:33:21 -0400

    Added functionality

--- Project A/numliteral.cpp
+++ Project A/numliteral.cpp
@@ -2,3 +2,81 @@
 // Created by djjac on 9/10/2020.
 //
 
+#define ASCII_ZERO 48
+
+#include <iostream>
+#include "numliteral.h"
+using namespace std;
+
+
+bool literal::isNum(char c)
+{
+    int digit = 0;
+    digit *= 10;
+    digit += c - ASCII_ZERO;
+    if (digit < 0 || digit > 9)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool literal::isSign(char c)
+{
+    int sign = c;
+    if (sign == 43 || sign == 45)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool literal::isExp(char c)
+{
+    if (c == 'e' || c == 'E')
+    {
+        return true;
+    }
+    return false;
+}
+
+bool literal::isLiteral(string number)
+{
+    bool exponent_state = false, decimal_state = false, start = true;
+    for (auto digit: number)
+    {
+        if (digit == '.')
+        {
+            if (exponent_state)
+            {
+                return false;
+            }
+            decimal_state = true;
+        }
+        else if (literal::isExp(digit))
+        {
+            if (start)
+            {
+                return false;
+            }
+            decimal_state = false;
+            exponent_state = true;
+        }
+        else if (literal::isSign(digit))
+        {
+            if (decimal_state)
+            {
+                return false;
+            }
+        }
+        else if (!literal::isNum(digit))
+        {
+            return false;
+        }
+        if (start)
+        {
+            start = false;
+        }
+    }
+    return true;
+}
--- Project A/numliteral.h
+++ Project A/numliteral.h
@@ -5,4 +5,14 @@
 #ifndef PROJECT_A_NUMLITERAL_H
 #define PROJECT_A_NUMLITERAL_H
 
+#include <iostream>
+
+namespace literal
+{
+    bool isNum(char c);
+    bool isSign(char c);
+    bool isExp(char c);
+    bool isLiteral(std::string number);
+}
+
 #endif //PROJECT_A_NUMLITERAL_H
